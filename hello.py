e4c3f9f05b0ece5158edccc2a9da4d0727c326b8
= <luis.latam@gmail.com>
2015-08-29 15:30:17 -0500

    First look at templates and login example pages with GET and POST http methods

--- hello.py
+++ hello.py
@@ -1,12 +1,24 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+	error = None
 	if request.method == "POST":
-		return "username is %s" % request.values["username"]
+		if valid_login(
+			request.form.get('username'),
+			request.form.get('password')
+		):
+			return "Welcome back, %s" % request.form.get('username')
+		else:	
+			error = "Incorrect username and password"
+	return render_template('login.html', error=error)
+
+def valid_login(username,password):
+	if username == password:
+		return True
 	else:
-		return '<form method="post" action="/login"><input type="text" name="username" /><p><button type="submit">Submit</button>'
+		return False
 
 if __name__ == "__main__":
 	app.debug = True
