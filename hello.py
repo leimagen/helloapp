07979c48d00c3d9daa307a5ba9e6c50147bdc0ad
= <luis.latam@gmail.com>
2015-08-28 23:04:11 -0500

    Last version of routes and url_for

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
+@app.route("/profile/<username>")
+def show_user_profile(username):
+	return "User: %s" % username
+
 @app.route("/")
-def hello_world():
-	return "Hello World!"
+def show_url_for():
+	return url_for("show_user_profile", username="Luiti")
 
 if __name__ == "__main__":
+	app.debug = True
 	app.run()
+
