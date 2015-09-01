d50894bc725d4b418ec4305a505f5298d52e83e0
= <luis.latam@gmail.com>
2015-08-31 22:34:12 -0500

    Included 'Sessions' in the app, now it's much safer.

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, flash, make_response
+from flask import Flask, request, render_template, redirect, url_for, flash, session
 import datetime
 
 app = Flask(__name__)
@@ -13,24 +13,21 @@ def login():
 			request.form.get('password')
 		):
 			flash("Successfully logged in!")
-			response = make_response(redirect(url_for('welcome')))
-			response.set_cookie('username', request.form.get('username'))
-			return response
+			session['username'] = request.form.get('username')
+			return redirect(url_for('welcome'))
 		else:	
 			error = "Incorrect username and password"
 	return render_template('login.html', error=error)
 
 @app.route("/logout")
 def logout():
-	response = make_response(redirect(url_for('login')))
-	response.set_cookie('username', '', expires=0)
-	return response
+	session.pop('username', None)
+	return redirect(url_for('login'))
 
 @app.route('/')
 def welcome():
-	username = request.cookies.get("username")
-	if username:
-		return render_template('welcome.html', username=username)
+	if 'username' in session:
+		return render_template('welcome.html', username=session['username'])
 	else:
 		return redirect(url_for('login'))
 
@@ -41,7 +38,7 @@ def valid_login(username,password):
 		return False
 
 if __name__ == "__main__":
-	app.secret_key = 'SuperSecretKey'
+	app.secret_key = '\x0b\x8b\xac\x8fa\xa0\xa4\x81\xc7G\x13\xad\xee\x9aW\xa7$\x02\x96\x88\xf5Q\xa2\x17'
 	app.debug = True
 	app.run()
 
